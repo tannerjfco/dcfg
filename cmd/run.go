cc9177e556a998d3ff92d43599dee06918c46eb8
Erin Corson <frodopwns@gmail.com>
2017-02-17 17:23:22 -0800

    moving dcfglib into pkg as dcfg, also fixing tests

--- cmd/run.go
+++ cmd/run.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/drud/dcfg/dcfglib"
+	"github.com/drud/dcfg/pkg/dcfg"
 	"github.com/spf13/cobra"
 )
 
@@ -45,7 +45,7 @@ var runCmd = &cobra.Command{
 			log.Fatalln("Could not read config file:", err)
 		}
 
-		groups, err := dcfglib.GetTaskSetList(fileBytes)
+		groups, err := dcfg.GetTaskSetList(fileBytes)
 		if err != nil {
 			log.Fatalln(err)
 		}
